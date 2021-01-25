f17dd679100af195b21b90b86518a7d491e15341
anthonym737 <69436689+anthonym737@users.noreply.github.com>
2021-01-25 19:20:16 +0100

    Add files via upload

---
+++ regex.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Adresse email valide</title>
+    </head>
+    <body>
+        <h2>Tu peux vérifier que ton adresse e-mail est valide.</h2>
+        <form method="post">
+            <label for="email">Ton email:</label><input id=email name=email><br />
+            <a href="verification.html"><input type="submit" value="verification"></a>
+        </form>
+        <?php include('main.php')?>
+    </body>
+</html>
